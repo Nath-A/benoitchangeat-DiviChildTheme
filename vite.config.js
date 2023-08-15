1effc815ee9ac4b38a3f3731e036117211d60d2a
Nath A <nathalie.anneessens@gmail.com>
2023-08-16 01:29:07 +0200

    Setup vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+
+export default {
+    // config options
+    build: {
+        outDir: "preview",
+        cssTarget: "chrome61",
+        minify: false,
+
+
+    },
+    css:
+    {
+        devSourcemap: true,
+    },
+
+}
